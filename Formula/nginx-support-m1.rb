da90a37d25bd2bd41b933c94eca5912f10e5d59f
tuanbui11 <buiquoctuan11@gmail.com>
2022-03-06 17:43:36 +0700

    custom nginx service

---
+++ Formula/nginx-support-m1.rb
@@ -0,0 +1,178 @@
+class Nginx < Formula
+    desc "HTTP(S) server and reverse proxy, and IMAP/POP3 proxy server"
+    homepage "https://nginx.org/"
+    # Use "mainline" releases only (odd minor version number), not "stable"
+    # See https://www.nginx.com/blog/nginx-1-12-1-13-released/ for why
+    url "https://nginx.org/download/nginx-1.21.6.tar.gz"
+    sha256 "66dc7081488811e9f925719e34d1b4504c2801c81dee2920e5452a86b11405ae"
+    license "BSD-2-Clause"
+    revision 1
+    head "https://hg.nginx.org/nginx/", using: :hg
+
+    livecheck do
+      url :homepage
+      regex(%r{nginx[._-]v?(\d+(?:\.\d+)+)</a>\nmainline version}i)
+    end
+
+    bottle do
+      sha256 arm64_monterey: "737ab2f1858bc8f3378f19da55e98d79f4014452e7ac37ab713b13c828ec95cc"
+      sha256 arm64_big_sur:  "3134aaef6f9b646784cc5dc5528958f57accff411e38feb137f2374e8d624ba9"
+      sha256 monterey:       "dba847687a67f4a25202f00c27821d2d39f0eddeb28fa8d3d1126e574706b042"
+      sha256 big_sur:        "4b6822266d64a0aa1baea76f01ead5ec163a90311747fa624cf9f234a7ee41b6"
+      sha256 catalina:       "e3f1eec4488b61a4c6bb56f338b30fbd1472a409db8a3bada92933fb92dc4b95"
+      sha256 x86_64_linux:   "1d64f3a67ccf10bb8e4529c148cc3244d7e2c8864126f3700fe57e775d7d6fd7"
+    end
+
+    depends_on "openssl@1.1"
+    depends_on "pcre2"
+
+    uses_from_macos "xz" => :build
+
+    def install
+      # keep clean copy of source for compiling dynamic modules e.g. passenger
+      (pkgshare/"src").mkpath
+      system "tar", "-cJf", (pkgshare/"src/src.tar.xz"), "."
+
+      # Changes default port to 8080
+      inreplace "conf/nginx.conf" do |s|
+        s.gsub! "listen       80;", "listen       8080;"
+        s.gsub! "    #}\n\n}", "    #}\n    include servers/*;\n}"
+      end
+
+      openssl = Formula["openssl@1.1"]
+      pcre = Formula["pcre2"]
+
+      cc_opt = "-I#{pcre.opt_include} -I#{openssl.opt_include}"
+      ld_opt = "-L#{pcre.opt_lib} -L#{openssl.opt_lib}"
+
+      args = %W[
+        --prefix=#/usr/local/bin
+        --sbin-path=#/usr/local/sbin/nginx
+        --with-cc-opt=#{cc_opt}
+        --with-ld-opt=#{ld_opt}
+        --conf-path=#/usr/local/ect/nginx/nginx.conf
+        --pid-path=#/usr/local/var/run/nginx.pid
+        --lock-path=#/usr/local/var/run/nginx.lock
+        --http-client-body-temp-path=#/usr/local/var/run/nginx/client_body_temp
+        --http-proxy-temp-path=#/usr/local/var/run/nginx/proxy_temp
+        --http-fastcgi-temp-path=#/usr/local/var/run/nginx/fastcgi_temp
+        --http-uwsgi-temp-path=#/usr/local/var/run/nginx/uwsgi_temp
+        --http-scgi-temp-path=#/usr/local/var/run/nginx/scgi_temp
+        --http-log-path=#/usr/local/var/log/nginx/access.log
+        --error-log-path=#/usr/local/var/log/nginx/error.log
+        --with-compat
+        --with-debug
+        --with-http_addition_module
+        --with-http_auth_request_module
+        --with-http_dav_module
+        --with-http_degradation_module
+        --with-http_flv_module
+        --with-http_gunzip_module
+        --with-http_gzip_static_module
+        --with-http_mp4_module
+        --with-http_random_index_module
+        --with-http_realip_module
+        --with-http_secure_link_module
+        --with-http_slice_module
+        --with-http_ssl_module
+        --with-http_stub_status_module
+        --with-http_sub_module
+        --with-http_v2_module
+        --with-ipv6
+        --with-mail
+        --with-mail_ssl_module
+        --with-pcre
+        --with-pcre-jit
+        --with-stream
+        --with-stream_realip_module
+        --with-stream_ssl_module
+        --with-stream_ssl_preread_module
+      ]
+
+      (pkgshare/"src/configure_args.txt").write args.join("\n")
+
+      if build.head?
+        system "./auto/configure", *args
+      else
+        system "./configure", *args
+      end
+
+      system "make", "install"
+      if build.head?
+        man8.install "docs/man/nginx.8"
+      else
+        man8.install "man/nginx.8"
+      end
+    end
+
+    def post_install
+      (etc/"nginx/servers").mkpath
+      (var/"run/nginx").mkpath
+
+      # nginx's docroot is #{prefix}/html, this isn't useful, so we symlink it
+      # to #{HOMEBREW_PREFIX}/var/www. The reason we symlink instead of patching
+      # is so the user can redirect it easily to something else if they choose.
+      html = prefix/"html"
+      dst = var/"www"
+
+      if dst.exist?
+        html.rmtree
+        dst.mkpath
+      else
+        dst.dirname.mkpath
+        html.rename(dst)
+      end
+
+      prefix.install_symlink dst => "html"
+
+      # for most of this formula's life the binary has been placed in sbin
+      # and Homebrew used to suggest the user copy the plist for nginx to their
+      # ~/Library/LaunchAgents directory. So we need to have a symlink there
+      # for such cases
+      sbin.install_symlink bin/"nginx" if rack.subdirs.any? { |d| d.join("sbin").directory? }
+    end
+
+    def caveats
+      <<~EOS
+        Docroot is: #/usr/local/var/www
+        The default port has been set in #/usr/local/etc/nginx/nginx.conf to 8080 so that
+        nginx can run without sudo.
+        nginx will load all files in #/usr/local/ect/nginx/servers/.
+      EOS
+    end
+
+    service do
+      if OS.linux?
+        run [opt_bin/"nginx", "-g", "'daemon off;'"]
+      else
+        run [opt_bin/"nginx", "-g", "daemon off;"]
+      end
+      keep_alive false
+      working_dir HOMEBREW_PREFIX
+    end
+
+    test do
+      (testpath/"nginx.conf").write <<~EOS
+        worker_processes 4;
+        error_log #{testpath}/error.log;
+        pid #{testpath}/nginx.pid;
+        events {
+          worker_connections 1024;
+        }
+        http {
+          client_body_temp_path #{testpath}/client_body_temp;
+          fastcgi_temp_path #{testpath}/fastcgi_temp;
+          proxy_temp_path #{testpath}/proxy_temp;
+          scgi_temp_path #{testpath}/scgi_temp;
+          uwsgi_temp_path #{testpath}/uwsgi_temp;
+          server {
+            listen 8080;
+            root #{testpath};
+            access_log #{testpath}/access.log;
+            error_log #{testpath}/error.log;
+          }
+        }
+      EOS
+      system bin/"nginx", "-t", "-c", testpath/"nginx.conf"
+    end
+  end
